b18bf48d5638d94b554f9436366938dc2b7aa527
Avah Xiao <avahxiao@gmail.com>
2026-08-20 21:47:44 -0700

    Rewrite mnist class a bit; separate training and testing methods

--- src/mlp/mnist.cpp
+++ src/mlp/mnist.cpp
@@ -1,5 +1,6 @@
-#include <cmath>
+#include <algorithm>
 #include <print>
+#include <random>
 #include <ranges>
 
 #include "mnist.h"
@@ -20,7 +21,8 @@ std::vector<float> to_float_vector(std::span<const uint8_t> data) {
 
 namespace mlp {
 
-mnist::mnist() : network({shared::TOTAL_PIXELS, 128, 128, shared::TOTAL_DIGITS}) {
+mnist::mnist()
+    : network({shared::TOTAL_PIXELS, 128, 128, shared::TOTAL_DIGITS}) {
   initialize_weights();
 }
 
@@ -28,70 +30,74 @@ mnist::mnist(const std::filesystem::path &file_path) : network(file_path) {
 }
 
 void mnist::train() {
-  shared::idx_matrix training_labels{"data/mnist/train-labels.idx"};
-  shared::idx_matrix training_images{"data/mnist/train-images.idx"};
+  const int epochs{10};
+  const std::int64_t batch_size{64};
+  const std::int64_t rows{training_labels_.rows()};
 
-  std::int64_t batch_size{64}; // TODO fix this
-  std::int64_t rows{training_labels.rows()};
+  const auto raw_inputs = to_float_vector(training_images_.data());
+  const auto raw_outputs =
+      training_labels_.data() | std::views::transform([](auto digit) {
+        std::array<float, shared::TOTAL_DIGITS> v{};
+        v.at(digit) = 1.0f;
+        return v;
+      }) |
+      std::views::join | std::ranges::to<std::vector<float>>();
 
-  auto raw_inputs = to_float_vector(training_images.data());
+  static std::random_device rd{};
+  static std::mt19937 gen{rd()};
 
-  auto raw_outputs = training_labels.data() |
-                     std::views::transform([](auto digit) {
-                       std::array<float, shared::TOTAL_DIGITS> v{};
-                       v.at(digit) = 1.0f;
-                       return v;
-                     }) |
-                     std::views::join | std::ranges::to<std::vector<float>>();
+  auto indices{std::views::iota(0, rows) | std::ranges::to<std::vector<int>>()};
 
-  const int iterations = 10;
+  for (int epoch{}; epoch < epochs; epoch++) {
+    std::shuffle(indices.begin(), indices.end(), gen);
+    float total_cost{};
+    for (std::int64_t i{}; i < rows; i += batch_size) {
+      const auto safe_batch_size{std::min(batch_size, rows - i)};
+      const auto batch_input_span =
+          raw_inputs | std::views::drop(i * shared::TOTAL_PIXELS);
+      const auto batch_output_span =
+          raw_outputs | std::views::drop(i * shared::TOTAL_DIGITS);
 
-  for (int it{0}; it < iterations; it++) {
-    for (std::int64_t i{0}; i < rows / batch_size; i++) {
-      auto batch_inputs =
-          raw_inputs | std::views::drop(i * batch_size * shared::TOTAL_PIXELS);
-      Eigen::Map<Eigen::MatrixXf> inputs(batch_inputs.data(), batch_size,
-                                         shared::TOTAL_PIXELS);
-
-      auto batch_outputs =
-          raw_outputs | std::views::drop(i * batch_size * shared::TOTAL_DIGITS);
-      Eigen::Map<Eigen::MatrixXf> outputs(batch_outputs.data(), batch_size,
-                                          shared::TOTAL_DIGITS);
+      Eigen::Map<const Eigen::MatrixXf> inputs{
+          batch_input_span.data(), safe_batch_size, shared::TOTAL_PIXELS};
+      Eigen::Map<const Eigen::MatrixXf> outputs{
+          batch_output_span.data(), safe_batch_size, shared::TOTAL_DIGITS};
 
       backpropagate(inputs, outputs);
     }
-    std::println("Training {}/{} iterations complete", it + 1, iterations);
+    std::println("Training {}/{} epochs complete; Current cost: ", epoch + 1,
+                 epochs, total_cost);
   }
+}
 
-  shared::idx_matrix testing_labels{"data/mnist/test-labels.idx"};
-  shared::idx_matrix testing_images{"data/mnist/test-images.idx"};
+void mnist::test() {
+  const auto raw_test_inputs = to_float_vector(testing_images_.data());
 
-  auto raw_test_inputs = to_float_vector(testing_images.data());
-
-  int correct{0};
-  for (int i = 0; i < testing_labels.rows(); i++) {
-    auto test_input_span =
+  int correct{};
+  for (int i{}; i < testing_labels_.rows(); i++) {
+    const auto test_input_span =
         raw_test_inputs | std::views::drop(shared::TOTAL_PIXELS * i);
-    Eigen::Map<Eigen::VectorXf> test_input(test_input_span.data(),
-                                           shared::TOTAL_PIXELS);
+    Eigen::Map<const Eigen::VectorXf> test_input{test_input_span.data(),
+                                                 shared::TOTAL_PIXELS};
     set_input(test_input);
     update();
 
-    int prediction = 0;
-    float largest = output()(0);
-    for (int digit{0}; digit < shared::TOTAL_DIGITS; digit++) {
+    int predicted_digit{};
+    float largest{output()(0)};
+    for (int digit{}; digit < shared::TOTAL_DIGITS; digit++) {
       if (output()(digit) > largest) {
-        prediction = digit;
+        predicted_digit = digit;
         largest = output()(digit);
       }
     }
-    if (prediction == static_cast<std::int8_t>(testing_labels.data().at(i))) {
+    if (predicted_digit ==
+        static_cast<std::int8_t>(testing_labels_.data().at(i))) {
       correct++;
     }
   }
 
   std::println("{}% accuracy", 100.0f * static_cast<float>(correct) /
-                                   static_cast<float>(testing_labels.rows()));
+                                   static_cast<float>(testing_labels_.rows()));
 }
 
 int mnist::predict(const Eigen::VectorXf &input) {
--- src/mlp/mnist.h
+++ src/mlp/mnist.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "mlp/network.h"
+#include "shared/idx_matrix.h"
 
 #include <filesystem>
 
@@ -12,7 +13,19 @@ public:
   explicit mnist(const std::filesystem::path &file_path);
 
   void train();
+  void test();
   int predict(const Eigen::VectorXf &input);
+
+private:
+  inline static const shared::idx_matrix training_labels_{
+      "data/mnist/train-labels.idx"};
+  inline static const shared::idx_matrix training_images_{
+      "data/mnist/train-images.idx"};
+
+  inline static const shared::idx_matrix testing_labels_{
+      "data/mnist/test-labels.idx"};
+  inline static const shared::idx_matrix testing_images_{
+      "data/mnist/test-images.idx"};
 };
 
 } // namespace mlp
