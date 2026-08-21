0975804f87ba354b07a36ee8190d5a24c9999e20
Avah Xiao <avahxiao@gmail.com>
2026-08-21 03:01:08 -0600

    Fix issue where eigen vector cannot be formatted
    For some reason works with clang but not gcc?

--- src/mlp/mnist.cpp
+++ src/mlp/mnist.cpp
@@ -114,9 +114,11 @@ int mnist::predict(const Eigen::VectorXf &input) {
   set_input(input);
   update();
 
-  auto output_vec = output();
+  const auto output_vec{output()};
+  const std::span output_span{output_vec.data(),
+                              static_cast<std::size_t>(output_vec.size())};
 
-  std::println("Output: {}", output_vec);
+  std::println("Output: {}", output_span);
 
   int predicted_digit{0};
   float max_activation{output_vec(0)};
